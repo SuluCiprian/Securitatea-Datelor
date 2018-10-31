2b91529de74c1c5f3503840365f92f8bac2cdab3
DESKTOP-HNIN6O2\Cipada <cipriansulu1@yahoo.com>
2018-10-31 17:46:45 +0200

    Lab 3

---
+++ Frequency/Frequency/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Frequency
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputText;
+            int[] appearances = new int[200];
+            float frequency;
+            int noLetters = 0;
+            inputText = Console.ReadLine();
+
+            foreach (var letter in inputText)
+            {
+                if (letter != ' ')
+                {
+                    appearances[Convert.ToInt32(letter)]++;
+                    noLetters++;
+                }
+            }
+
+            foreach (var letter in inputText)
+            {
+                frequency = (float) appearances[Convert.ToInt32(letter)] / noLetters;
+                if (letter != ' ')
+                {
+                    Console.WriteLine(letter + " - " + frequency);
+                }
+            }
+        }
+    }
+}
